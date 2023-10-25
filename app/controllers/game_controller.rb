1a74a84d683e07032f67c61781f8d109e9d0e9b6
btlim17 <146046964+btlim17@users.noreply.github.com>
2023-10-25 14:54:28 +0000

    rails_rps

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,47 @@
+class GameController < ApplicationController
+  def player_moves
+    render({:template => "game_templates/player_moves"})
+  end
+  def user_played_rock
+    moves = ["rock", "paper", "scissors"]
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "We tied!"
+    elsif @computer_move == "paper"
+      @outcome = "We lost!"
+    else 
+      @outcome = "We won!"
+    end
+
+    render({:template => "game_templates/play_rock"})
+  end
+  def user_played_paper
+    moves = ["rock", "paper", "scissors"]
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "We win!"
+    elsif @computer_move == "paper"
+      @outcome = "We tied!"
+    else 
+      @outcome = "We lose!"
+    end
+
+    render({:template => "game_templates/play_paper"})
+  end
+
+  def user_played_scissors
+    moves = ["rock", "paper", "scissors"]
+    @computer_move = moves.sample
+    if @computer_move == "rock"
+      @outcome = "We lose!"
+    elsif @computer_move == "paper"
+      @outcome = "We win!"
+    else 
+      @outcome = "We tied"
+    end
+
+  render({:template => "game_templates/play_scissors"})
+  end
+end
